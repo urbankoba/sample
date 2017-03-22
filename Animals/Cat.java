bdb60522ad002989c16c4a0c1bc697f576b9e0a2
urbankoba <urban.koba@gmail.com>
2017-03-23 02:55:39 +0900

    Update Cat.java

--- Animals/Cat.java
+++ Animals/Cat.java
@@ -2,8 +2,7 @@ package Animals;
 public class Cat extends Animal{
 
 	@Override
-	String eat(String food) {
-		System.out.println(food);
-		return food;
+	void eat() {
+		System.out.println("キャットフードを食べます");
 	}
 }
